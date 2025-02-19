fc4ec9f0809cd31820fee2365825e1a11a651c4d
Mihir2423 <mihirraj444@gmail.com>
2025-02-20 00:32:11 +0530

    feat: add project detail page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Provider from "./provider";
+import { TailwindIndicator } from "@/components/globals/tailwind-indicator";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,11 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Provider>{children}</Provider>
+        <Provider>
+          {children}
+
+          <TailwindIndicator />
+        </Provider>
       </body>
     </html>
   );
