766603b8fe414b0e17579cb22149495882f55907
Mihir2423 <mihirraj444@gmail.com>
2025-03-01 05:45:27 +0530

    fix: metadata

--- src/lib/metadata.ts
+++ src/lib/metadata.ts
@@ -4,9 +4,9 @@ import getMetaData from "metadata-scraper";
 export async function extractMetadata(url: string) {
   try {
     const data = await getMetaData(url);
-    return data;
+    return { data, error: null };
   } catch (error) {
     console.error("Error extracting metadata:", error);
-    return null;
+    return { data: null, error };
   }
 }
